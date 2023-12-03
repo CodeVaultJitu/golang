238f285028f449a370c0de33bc0192ef570c44a4
j1tu <jituballavhazarika@gmail.com>
2023-12-03 20:24:22 +0530

    updated
    use of logical operator AND

--- allTests/ifElse/main.go
+++ allTests/ifElse/main.go
@@ -3,16 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	age := 10
-	if age > 60 {
-		fmt.Println("gettig older")
-	} else if age > 30 {
-		fmt.Println("Getting wiser")
-	} else if age > 20 {
-		fmt.Println("Adulthood")
-	} else if age > 10 {
-		fmt.Println("Young blood")
+	// age := 10
+	// if age > 60 {
+	// 	fmt.Println("gettig older")
+	// } else if age > 30 {
+	// 	fmt.Println("Getting wiser")
+	// } else if age > 20 {
+	// 	fmt.Println("Adulthood")
+	// } else if age > 10 {
+	// 	fmt.Println("Young blood")
+	// } else {
+	// 	fmt.Println("Booting up")
+	// }
+
+	isSphere, radius := true, 200
+	var big bool
+
+	big = isSphere && radius >= 200
+
+	if big {
+		fmt.Println("It's a big sphere.")
 	} else {
-		fmt.Println("Booting up")
+		fmt.Println("I don't know.")
 	}
+
+
 }
