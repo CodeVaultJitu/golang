6125a3f8b0200e891714395e67db306ebac091f7
j1tu <jituballavhazarika@gmail.com>
2023-12-04 12:54:29 +0530

    q
    quoted text print

--- allTests/ifElse/main.go
+++ allTests/ifElse/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	// age := 10
@@ -16,16 +19,29 @@ func main() {
 	// 	fmt.Println("Booting up")
 	// }
 
-	isSphere, radius := true, 200
-	var big bool
+	// isSphere, radius := true, 200
+	// var big bool
 
-	big = isSphere && radius >= 200
+	// big = isSphere && radius >= 200
 
-	if big {
-		fmt.Println("It's a big sphere.")
-	} else {
-		fmt.Println("I don't know.")
-	}
+	// if big {
+	// 	fmt.Println("It's a big sphere.")
+	// } else {
+	// 	fmt.Println("I don't know.")
+	// }
 
+		args := os.Args[1:]
+		count := len(args)
 
+		if count < 1 {
+			fmt.Println("Give me args")
+		}else if count == 1 {
+			//q is for quoted outputs, without args[] it will print with sq bracks to represent
+			//the slice of array in which it is stored
+			fmt.Printf("There is one: %q", args[0])
+		} else if count == 2 {
+				fmt.Printf("There are 2: %q", args[0]+" "+args[1])
+		} else {
+			fmt.Printf("There are %d arguments", count)
+		}
 }
