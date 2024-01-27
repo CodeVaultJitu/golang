8a06b862b7044de1f712c5a4beff34f2309ec0d3
j1tu <jituballavhazarika@gmail.com>
2024-01-27 15:21:21 +0530

    for if loops

---
+++ allTests/for/sumOnlyEvens/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	if len(os.Args) != 3 {
+		fmt.Println(`Please enter 2 numbers
+The first number should be less than the 2nd number`)
+		return
+	}
+
+	min, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Please enter 2 numbers")
+		return
+	}
+
+	max, errr := strconv.Atoi(os.Args[2])
+	if errr != nil {
+		fmt.Println("Please enter 2 numbers")
+		return
+	}
+
+	if min < max {
+		var plus int
+		for i := min; i <= max; i++ {
+			if i%2 != 0 {
+				continue
+			}
+
+			plus += i
+
+			if i == max-1 {
+				fmt.Printf("%d = ", i)
+				//printing = after the last number
+			} else if i == max {
+				fmt.Printf("%d = ", i)
+			} else {
+				fmt.Printf("%d + ", i)
+				// will print + after every number until
+			}
+
+		}
+		fmt.Println(plus)
+	} else {
+		fmt.Println("The first number should be less than the 2nd number")
+		return
+	}
+
+}
